6a6c014bbf8f0cd69749fc9ee7537182d108aa7b
Gloriaza Odyardy <gloriazalawrance@gmail.com>
2025-07-13 11:25:42 +0700

    Restructures layout and stying for pricing table component

---
+++ src/components/Card.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="pricing__table">
+        <div class="pricing__table__plan">
+            <div></div>
+            <div>
+                <p>Free</p>
+                <p>$0</p>
+                <p>Per month</p>
+            </div>
+            <div>
+                <p>Team</p>
+                <p>$59</p>
+                <p>Per month</p>
+            </div>
+            <div>
+                <p>Popular</p>
+                <p>$99</p>
+                <p>Per month</p>
+            </div>
+            <div>
+                <p>Enterprise</p>
+                <p>$299</p>
+                <p>Per month</p>
+            </div>
+        </div>
+        <div class="pricing__table__details">
+            <div>
+                <p>Website number</p>
+                <p>Server storage</p>
+                <p>Database</p>
+                <p>Unmetered Bandwidth</p>
+                <p>SSD Disk</p>
+                <p>VCPUS Fontworld</p>
+                <p>Wordpress install</p>
+                <p>Server speed</p>
+            </div>
+            <div>
+                <p>01</p>
+                <p>100GB</p>
+                <p>-</p>
+                <p>-</p>
+                <p>-</p>
+                <p>-</p>
+                <p>-</p>
+                <p>-</p>
+            </div>
+            <div>
+                <p>10</p>
+                <p>500GB</p>
+                <p>15</p>
+                <p><img src="/resources/check-1.svg" alt="check icon"></p>
+                <p>-</p>
+                <p>-</p>
+                <p>-</p>
+                <p>-</p>
+            </div>
+            <div>
+                <p>50</p>
+                <p>1TB</p>
+                <p>Unlimited</p>
+                <p><img src="/resources/check.svg" alt="check icon"></p>
+                <p><img src="/resources/check.svg" alt="check icon"></p>
+                <p><img src="/resources/check.svg" alt="check icon"></p>
+                <p><img src="/resources/check.svg" alt="check icon"></p>
+                <p><img src="/resources/check.svg" alt="check icon"></p>
+            </div>
+            <div>
+                <p>Unlimited</p>
+                <p>Unlimited</p>
+                <p>Unlimited</p>
+                <p><img src="/resources/check-1.svg" alt="check icon"></p>
+                <p><img src="/resources/check-1.svg" alt="check icon"></p>
+                <p><img src="/resources/check-1.svg" alt="check icon"></p>
+                <p><img src="/resources/check-1.svg" alt="check icon"></p>
+                <p><img src="/resources/check-1.svg" alt="check icon"></p>
+            </div>
+        </div>
+        <div class="pricing__table__cta">
+            <div></div>
+            <a class="get-started">Get Started <img src="/resources/down arrow-1.svg" alt="right arrow"></a>
+            <a class="get-started">Get Started <img src="/resources/down arrow-1.svg" alt="right arrow"></a>
+            <a class="get-started">Get Started <img src="/resources/down arrow.svg" alt="right arrow"></a>
+            <a class="get-started">Get Started <img src="/resources/down arrow-1.svg" alt="right arrow"></a>
+        </div>
+    </div>
+</template>
---
+++ src/components/Hero.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="hero">
+        <h1 class="text-heading">Pricing & Plans</h1>
+        <p class="text-body">Discover the plan that unlocks the transformative power of digital art and join our vibrant community today.
+        </p>
+    </div>
+</template>
