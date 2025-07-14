bc42937250c6c4f1f3ec5faf50d631c914e4c56b
Gloriaza Odyardy <gloriazalawrance@gmail.com>
2025-07-14 21:16:36 +0700

    Refactor pricing table layout and styling, update README.md

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,29 +1,7 @@
 <template>
     <div class="pricing__table">
-        <div class="pricing__table__plan">
+        <div class="pricing__table__sticky">
             <div></div>
-            <div>
-                <p>Free</p>
-                <p>$0</p>
-                <p>Per month</p>
-            </div>
-            <div>
-                <p>Team</p>
-                <p>$59</p>
-                <p>Per month</p>
-            </div>
-            <div>
-                <p>Popular</p>
-                <p>$99</p>
-                <p>Per month</p>
-            </div>
-            <div>
-                <p>Enterprise</p>
-                <p>$299</p>
-                <p>Per month</p>
-            </div>
-        </div>
-        <div class="pricing__table__details">
             <div>
                 <p>Website number</p>
                 <p>Server storage</p>
@@ -34,53 +12,79 @@
                 <p>Wordpress install</p>
                 <p>Server speed</p>
             </div>
-            <div>
-                <p>01</p>
-                <p>100GB</p>
-                <p>-</p>
-                <p>-</p>
-                <p>-</p>
-                <p>-</p>
-                <p>-</p>
-                <p>-</p>
-            </div>
-            <div>
-                <p>10</p>
-                <p>500GB</p>
-                <p>15</p>
-                <p><img src="/resources/check-1.svg" alt="check icon"></p>
-                <p>-</p>
-                <p>-</p>
-                <p>-</p>
-                <p>-</p>
-            </div>
-            <div>
-                <p>50</p>
-                <p>1TB</p>
-                <p>Unlimited</p>
-                <p><img src="/resources/check.svg" alt="check icon"></p>
-                <p><img src="/resources/check.svg" alt="check icon"></p>
-                <p><img src="/resources/check.svg" alt="check icon"></p>
-                <p><img src="/resources/check.svg" alt="check icon"></p>
-                <p><img src="/resources/check.svg" alt="check icon"></p>
-            </div>
-            <div>
-                <p>Unlimited</p>
-                <p>Unlimited</p>
-                <p>Unlimited</p>
-                <p><img src="/resources/check-1.svg" alt="check icon"></p>
-                <p><img src="/resources/check-1.svg" alt="check icon"></p>
-                <p><img src="/resources/check-1.svg" alt="check icon"></p>
-                <p><img src="/resources/check-1.svg" alt="check icon"></p>
-                <p><img src="/resources/check-1.svg" alt="check icon"></p>
-            </div>
-        </div>
-        <div class="pricing__table__cta">
             <div></div>
-            <a class="get-started">Get Started <img src="/resources/down arrow-1.svg" alt="right arrow"></a>
-            <a class="get-started">Get Started <img src="/resources/down arrow-1.svg" alt="right arrow"></a>
-            <a class="get-started">Get Started <img src="/resources/down arrow.svg" alt="right arrow"></a>
-            <a class="get-started">Get Started <img src="/resources/down arrow-1.svg" alt="right arrow"></a>
+        </div>
+        <div class="pricing__table__scroll">
+            <div class="pricing__table__scroll__plan">
+                <div>
+                    <p>Free</p>
+                    <p>$0</p>
+                    <p>Per month</p>
+                </div>
+                <div>
+                    <p>Team</p>
+                    <p>$59</p>
+                    <p>Per month</p>
+                </div>
+                <div>
+                    <p>Popular</p>
+                    <p>$99</p>
+                    <p>Per month</p>
+                </div>
+                <div>
+                    <p>Enterprise</p>
+                    <p>$299</p>
+                    <p>Per month</p>
+                </div>
+            </div>
+            <div class="pricing__table__scroll__details">
+                <div>
+                    <p>01</p>
+                    <p>100GB</p>
+                    <p>-</p>
+                    <p>-</p>
+                    <p>-</p>
+                    <p>-</p>
+                    <p>-</p>
+                    <p>-</p>
+                </div>
+                <div>
+                    <p>10</p>
+                    <p>500GB</p>
+                    <p>15</p>
+                    <p><img src="/resources/check-1.svg" alt="check icon"></p>
+                    <p>-</p>
+                    <p>-</p>
+                    <p>-</p>
+                    <p>-</p>
+                </div>
+                <div>
+                    <p>50</p>
+                    <p>1TB</p>
+                    <p>Unlimited</p>
+                    <p><img src="/resources/check.svg" alt="check icon"></p>
+                    <p><img src="/resources/check.svg" alt="check icon"></p>
+                    <p><img src="/resources/check.svg" alt="check icon"></p>
+                    <p><img src="/resources/check.svg" alt="check icon"></p>
+                    <p><img src="/resources/check.svg" alt="check icon"></p>
+                </div>
+                <div>
+                    <p>Unlimited</p>
+                    <p>Unlimited</p>
+                    <p>Unlimited</p>
+                    <p><img src="/resources/check-1.svg" alt="check icon"></p>
+                    <p><img src="/resources/check-1.svg" alt="check icon"></p>
+                    <p><img src="/resources/check-1.svg" alt="check icon"></p>
+                    <p><img src="/resources/check-1.svg" alt="check icon"></p>
+                    <p><img src="/resources/check-1.svg" alt="check icon"></p>
+                </div>
+            </div>
+            <div class="pricing__table__scroll__cta">
+                <a class="get-started">Get Started <img src="/resources/down arrow-1.svg" alt="right arrow"></a>
+                <a class="get-started">Get Started <img src="/resources/down arrow-1.svg" alt="right arrow"></a>
+                <a class="get-started">Get Started <img src="/resources/down arrow.svg" alt="right arrow"></a>
+                <a class="get-started">Get Started <img src="/resources/down arrow-1.svg" alt="right arrow"></a>
+            </div>
         </div>
     </div>
 </template>
